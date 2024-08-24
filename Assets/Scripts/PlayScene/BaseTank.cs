c0a7d9359407e31a2920c95b220bca965317768a
notforget <1461004276@qq.com>
2024-08-25 01:56:03 +0800

    reup

---
+++ Assets/Scripts/PlayScene/BaseTank.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseTank: MonoBehaviour
+{
+    public int atk;
+    public int def;
+    public int nowhp;
+    public int maxHp;
+    
+    public float moveSpeed;
+    public float rotateSpeed;
+    public float headRotateSpeed;
+
+
+    public void Fire()
+    {
+        
+    }
+
+    public void Hurt()
+    {
+        
+    }
+    
+    
+    
+}
